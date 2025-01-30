262cec3f649f8865ecb77696c8dd3e47c5b50890
oddguid <george.mathijssen@gmail.com>
2025-01-30 20:39:26 +0100

    Added unit tests for TextNode

---
+++ src/test_textnode.py
@@ -0,0 +1,54 @@
+import unittest
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_ctor_no_url(self):
+        text = "This is a text node"
+        text_type = TextType.CODE
+
+        node = TextNode(text, text_type)
+
+        self.assertEqual(node.text, text)
+        self.assertEqual(node.text_type, text_type)
+        self.assertEqual(node.url, None)
+
+    def test_ctor_url(self):
+        text = "This is a text node"
+        text_type = TextType.CODE
+        url = "https://example.com"
+
+        node = TextNode(text, text_type, url)
+
+        self.assertEqual(node.text, text)
+        self.assertEqual(node.text_type, text_type)
+        self.assertEqual(node.url, url)
+
+    def test_eq(self):
+        node1 = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        
+        self.assertEqual(node1, node2)
+
+    def test_eq_text_diff(self):
+        node1 = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("__DIFF__", TextType.BOLD)
+        
+        self.assertNotEqual(node1, node2)
+
+    def test_eq_texttype_diff(self):
+        node1 = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.NORMAL)
+        
+        self.assertNotEqual(node1, node2)
+
+    def test_eq_url_diff(self):
+        node1 = TextNode("This is a text node", TextType.CODE)
+        node2 = TextNode("This is a text node", TextType.CODE, "example.com")
+        node3 = TextNode("This is a text node", TextType.CODE, "foo.com")
+        
+        self.assertNotEqual(node1, node2)
+        self.assertNotEqual(node1, node3)
+        self.assertNotEqual(node2, node3)
+
+if __name__ == "__main__":
+    unittest.main()
