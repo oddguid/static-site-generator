eb4934672bede7eb633f619e8b649cfc5cce7d68
oddguid <george.mathijssen@gmail.com>
2025-02-02 19:18:04 +0100

    Added to split on delimiter, update unit tests

--- src/convert.py
+++ src/convert.py
@@ -19,3 +19,33 @@ def text_node_to_html_node(text_node):
             return LeafNode("img", "", { "src": text_node.url, "alt": text_node.text })
         case _:
             raise Exception("Invalid HTML: text type invalid")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.NORMAL:
+            new_nodes.append(old_node)
+            continue
+
+        split_nodes = []
+        parts = old_node.text.split(delimiter)
+
+        if len(parts) % 2 == 0:
+            # no closing delimiter
+            raise Exception("Invalid Markdown: no closing delimiter found")
+
+        for i in range(len(parts)):
+            if parts[i] == "":
+                # skip empty parts
+                continue
+
+            if i % 2 == 0:
+                # normal text section
+                split_nodes.append(TextNode(parts[i], TextType.NORMAL))
+            else:
+                split_nodes.append(TextNode(parts[i], text_type))
+
+        new_nodes.extend(split_nodes)
+
+    return new_nodes
--- src/test_convert.py
+++ src/test_convert.py
@@ -1,5 +1,5 @@
 import unittest
-from convert import text_node_to_html_node
+from convert import text_node_to_html_node, split_nodes_delimiter
 from leafnode import LeafNode
 from textnode import TextNode, TextType
 
@@ -77,6 +77,80 @@ class TestConvert(unittest.TestCase):
         self.assertEqual(isinstance(result, LeafNode), True)
         self.assertEqual(result.to_html(),
                          "<img src=\"example.com/foo.png\" alt=\"alt text\"></img>")
+
+    def test_split_nodes_delimiter_bold(self):
+        node = TextNode("This is **bolded** text", TextType.NORMAL)
+
+        result = split_nodes_delimiter([node], "**", TextType.BOLD)
+
+        self.assertListEqual(result,
+            [
+                TextNode("This is ", TextType.NORMAL),
+                TextNode("bolded", TextType.BOLD),
+                TextNode(" text", TextType.NORMAL)
+            ])
+
+    def test_split_nodes_delimiter_bold_double(self):
+        node = TextNode("Some **bolded** text with **another**", TextType.NORMAL)
+
+        result = split_nodes_delimiter([node], "**", TextType.BOLD)
+
+        self.assertListEqual(result,
+            [
+                TextNode("Some ", TextType.NORMAL),
+                TextNode("bolded", TextType.BOLD),
+                TextNode(" text with ", TextType.NORMAL),
+                TextNode("another", TextType.BOLD)
+            ])
+
+    def test_split_nodes_delimiter_bold_multiword(self):
+        node = TextNode("Some text with **bolded words**", TextType.NORMAL)
+
+        result = split_nodes_delimiter([node], "**", TextType.BOLD)
+
+        self.assertListEqual(result,
+            [
+                TextNode("Some text with ", TextType.NORMAL),
+                TextNode("bolded words", TextType.BOLD)
+            ])
+
+    def test_split_nodes_delimiter_italic_multiword(self):
+        node = TextNode("Some text with *italic words*", TextType.NORMAL)
+
+        result = split_nodes_delimiter([node], "*", TextType.ITALIC)
+
+        self.assertListEqual(result,
+            [
+                TextNode("Some text with ", TextType.NORMAL),
+                TextNode("italic words", TextType.ITALIC)
+            ])
+
+    def test_split_nodes_delimiter_bold_and_italic(self):
+        node = TextNode("Some text with **bolded words** and *italic* word", TextType.NORMAL)
+
+        result = split_nodes_delimiter([node], "**", TextType.BOLD)
+        result = split_nodes_delimiter(result, "*", TextType.ITALIC)
+
+        self.assertListEqual(result,
+            [
+                TextNode("Some text with ", TextType.NORMAL),
+                TextNode("bolded words", TextType.BOLD),
+                TextNode(" and ", TextType.NORMAL),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word", TextType.NORMAL)
+            ])
+
+
+    def test_split_nodes_delimiter_code(self):
+        node = TextNode("Some text with `code block`", TextType.NORMAL)
+
+        result = split_nodes_delimiter([node], "`", TextType.CODE)
+
+        self.assertListEqual(result,
+            [
+                TextNode("Some text with ", TextType.NORMAL),
+                TextNode("code block", TextType.CODE)
+            ])
 
 if __name__ == "__main__":
     unittest.main()
