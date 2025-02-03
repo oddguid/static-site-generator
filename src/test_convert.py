edfb9d0edcb456d393823501f63726c857d3b720
oddguid <george.mathijssen@gmail.com>
2025-02-03 19:24:30 +0100

    Added functions to split on images and links, update unit tests

--- src/test_convert.py
+++ src/test_convert.py
@@ -1,5 +1,12 @@
 import unittest
-from convert import text_node_to_html_node, split_nodes_delimiter
+from convert import (
+        text_node_to_html_node,
+        split_nodes_delimiter,
+        extract_markdown_images,
+        extract_markdown_links,
+        split_nodes_image,
+        split_nodes_link
+    )
 from leafnode import LeafNode
 from textnode import TextNode, TextType
 
@@ -150,6 +157,206 @@ class TestConvert(unittest.TestCase):
             [
                 TextNode("Some text with ", TextType.NORMAL),
                 TextNode("code block", TextType.CODE)
+            ])
+
+    def test_extract_markdown_images_none(self):
+        text = "this is text without images"
+
+        result = extract_markdown_images(text)
+
+        self.assertEqual(len(result), 0)
+
+    def test_extract_markdown_images_single(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif)"
+
+        result = extract_markdown_images(text)
+
+        self.assertListEqual(result,
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif")
+            ])
+
+    def test_extract_markdown_images_multiple(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+
+        result = extract_markdown_images(text)
+
+        self.assertListEqual(result,
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")
+            ])
+
+    def test_extract_markdown_images_not_image(self):
+        text = "This is text with a [rick roll](https://i.imgur.com/aKaOqIh.gif)"
+
+        result = extract_markdown_images(text)
+
+        self.assertEqual(len(result), 0)
+
+    def test_extract_markdown_links_none(self):
+        text = "sample text, no link"
+
+        result = extract_markdown_links(text)
+
+        self.assertEqual(len(result), 0)
+
+    def test_extract_markdown_links_single(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev)"
+
+        result = extract_markdown_links(text)
+
+        self.assertListEqual(result,
+            [
+                ("to boot dev", "https://www.boot.dev")
+            ])
+
+    def test_extract_markdown_links_multiple(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+
+        result = extract_markdown_links(text)
+
+        self.assertListEqual(result,
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev")
+            ])
+
+    def test_extract_markdown_links_not_link(self):
+        text = "This is text with a link ![to boot dev](https://www.boot.dev)"
+
+        result = extract_markdown_links(text)
+
+        self.assertEqual(len(result), 0)
+
+    def test_split_nodes_image_no_image(self):
+        node = TextNode("sample text", TextType.NORMAL)
+
+        result = split_nodes_image([node])
+
+        self.assertListEqual(result,
+            [
+                TextNode("sample text", TextType.NORMAL)
+            ])
+
+    def test_split_nodes_image_single(self):
+        node1 = TextNode("sample ![alt text](image.gif) image", TextType.NORMAL)
+        node2 = TextNode("simple bold", TextType.BOLD)
+
+        result = split_nodes_image([node1, node2])
+
+        self.assertListEqual(result,
+            [
+                TextNode("sample ", TextType.NORMAL),
+                TextNode("alt text", TextType.IMAGE, "image.gif"),
+                TextNode(" image", TextType.NORMAL),
+                TextNode("simple bold", TextType.BOLD)
+            ])
+
+    def test_split_nodes_image_single_start(self):
+        node1 = TextNode("![alt text](image.gif) image", TextType.NORMAL)
+        node2 = TextNode("simple bold", TextType.BOLD)
+
+        result = split_nodes_image([node1, node2])
+
+        self.assertListEqual(result,
+            [
+                TextNode("alt text", TextType.IMAGE, "image.gif"),
+                TextNode(" image", TextType.NORMAL),
+                TextNode("simple bold", TextType.BOLD)
+            ])
+
+    def test_split_nodes_image_single_end(self):
+        node1 = TextNode("sample ![alt text](image.gif)", TextType.NORMAL)
+        node2 = TextNode("simple bold", TextType.BOLD)
+
+        result = split_nodes_image([node1, node2])
+
+        self.assertListEqual(result,
+            [
+                TextNode("sample ", TextType.NORMAL),
+                TextNode("alt text", TextType.IMAGE, "image.gif"),
+                TextNode("simple bold", TextType.BOLD)
+            ])
+
+    def test_split_nodes_image_multiple(self):
+        node1 = TextNode("sample ![alt text](image.gif) ![text alt](pic.png)", TextType.NORMAL)
+        node2 = TextNode("simple bold", TextType.BOLD)
+
+        result = split_nodes_image([node1, node2])
+
+        self.assertListEqual(result,
+            [
+                TextNode("sample ", TextType.NORMAL),
+                TextNode("alt text", TextType.IMAGE, "image.gif"),
+                TextNode(" ", TextType.NORMAL),
+                TextNode("text alt", TextType.IMAGE, "pic.png"),
+                TextNode("simple bold", TextType.BOLD)
+            ])
+
+    def test_split_nodes_link_no_link(self):
+        node = TextNode("sample text", TextType.NORMAL)
+
+        result = split_nodes_link([node])
+
+        self.assertListEqual(result,
+            [
+                TextNode("sample text", TextType.NORMAL)
+            ])
+
+    def test_split_nodes_link_single(self):
+        node1 = TextNode("sample [link](example.com) image", TextType.NORMAL)
+        node2 = TextNode("simple bold", TextType.BOLD)
+
+        result = split_nodes_link([node1, node2])
+
+        self.assertListEqual(result,
+            [
+                TextNode("sample ", TextType.NORMAL),
+                TextNode("link", TextType.LINK, "example.com"),
+                TextNode(" image", TextType.NORMAL),
+                TextNode("simple bold", TextType.BOLD)
+            ])
+
+    def test_split_nodes_link_single_start(self):
+        node1 = TextNode("[link](example.com) image", TextType.NORMAL)
+        node2 = TextNode("simple bold", TextType.BOLD)
+
+        result = split_nodes_link([node1, node2])
+
+        self.assertListEqual(result,
+            [
+                TextNode("link", TextType.LINK, "example.com"),
+                TextNode(" image", TextType.NORMAL),
+                TextNode("simple bold", TextType.BOLD)
+            ])
+
+    def test_split_nodes_link_single_end(self):
+        node1 = TextNode("sample [link](example.com)", TextType.NORMAL)
+        node2 = TextNode("simple bold", TextType.BOLD)
+
+        result = split_nodes_link([node1, node2])
+
+        self.assertListEqual(result,
+            [
+                TextNode("sample ", TextType.NORMAL),
+                TextNode("link", TextType.LINK, "example.com"),
+                TextNode("simple bold", TextType.BOLD)
+            ])
+
+    def test_split_nodes_link_multiple(self):
+        node1 = TextNode("sample [link](example.com) [zelda](w3.org)", TextType.NORMAL)
+        node2 = TextNode("simple bold", TextType.BOLD)
+
+        result = split_nodes_link([node1, node2])
+
+        self.assertListEqual(result,
+            [
+                TextNode("sample ", TextType.NORMAL),
+                TextNode("link", TextType.LINK, "example.com"),
+                TextNode(" ", TextType.NORMAL),
+                TextNode("zelda", TextType.LINK, "w3.org"),
+                TextNode("simple bold", TextType.BOLD)
             ])
 
 if __name__ == "__main__":
